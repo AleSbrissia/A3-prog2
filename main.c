134675911dedb10928b23c75a739a4414deaedaf
alexandre-sbrissia <alexandresbrissia1@gmail.com>
2025-11-24 20:57:03 -0300

    trocando tab por space

--- main.c
+++ main.c
@@ -14,205 +14,205 @@
 
 int main() {
 
-	al_init();	
-	al_init_primitives_addon();	
-	al_init_font_addon() ;
-	al_install_keyboard();
-	al_init_image_addon();
-	al_init_ttf_addon();
+    al_init();	
+    al_init_primitives_addon();	
+    al_init_font_addon() ;
+    al_install_keyboard();
+    al_init_image_addon();
+    al_init_ttf_addon();
 
-	ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);	
-	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();	
-	ALLEGRO_FONT* font = NULL ;	
-	ALLEGRO_DISPLAY* disp = al_create_display(X_SCREEN, Y_SCREEN);
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);	
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();	
+    ALLEGRO_FONT* font = NULL ;	
+    ALLEGRO_DISPLAY* disp = al_create_display(X_SCREEN, Y_SCREEN);
     ALLEGRO_BITMAP* game_bg_img = al_load_bitmap("assets/bg.png") ;
 
-	if(!timer || !queue || !disp)
-		return -1 ;	
+    if(!timer || !queue || !disp)
+        return -1 ;	
 
-	al_register_event_source(queue, al_get_keyboard_event_source());
-	al_register_event_source(queue, al_get_display_event_source(disp));
-	al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
 
-	if (!game_bg_img) {       
+    if (!game_bg_img) {       
 
-		fprintf(stderr, "erro carregamento de sprite\n") ;
-	}
+        fprintf(stderr, "erro carregamento de sprite\n") ;
+    }
 
-	font = al_load_font("assets/Daydream.ttf", 36, 0) ;
-	if (!font) {
+    font = al_load_font("assets/Daydream.ttf", 36, 0) ;
+    if (!font) {
 
-		fprintf(stderr, "NAO TEM FONTE\n") ;
-		font = al_create_builtin_font() ;
-		if(!font)
-			return -1 ;
-	}
+        fprintf(stderr, "NAO TEM FONTE\n") ;
+        font = al_create_builtin_font() ;
+        if(!font)
+            return -1 ;
+    }
 
-	player* p1 = NULL ;
-	square* floor = NULL ;
+    player* p1 = NULL ;
+    square* floor = NULL ;
     obstacle_manager* obs_manager = NULL ;
-	platform_manager* plat_manager = NULL ;
+    platform_manager* plat_manager = NULL ;
     game_state estado = MENU ;
-	bool done = false;
-	bool redraw = true ; 
-	int menu_select = 1 ;
+    bool done = false;
+    bool redraw = true ; 
+    int menu_select = 1 ;
 
     game_set(&p1, &floor, &obs_manager, &plat_manager) ;
 
-	al_start_timer(timer);
+    al_start_timer(timer);
 
-	while(!done){
-		ALLEGRO_EVENT event;
-		al_wait_for_event(queue, &event);
+    while(!done){
+        ALLEGRO_EVENT event;
+        al_wait_for_event(queue, &event);
 
         switch (event.type)
-		{
-			case ALLEGRO_EVENT_TIMER :
+        {
+            case ALLEGRO_EVENT_TIMER :
                 
-				if(estado == GAMEPLAY) {
-					player_update_movement(p1, 1.0/FPS, floor, plat_manager) ;
-					obstacle_manager_update(obs_manager, 1.0/FPS, p1, X_SCREEN, Y_FLOOR, GRAVITY) ;
+                if(estado == GAMEPLAY) {
+                    player_update_movement(p1, 1.0/FPS, floor, plat_manager) ;
+                    obstacle_manager_update(obs_manager, 1.0/FPS, p1, X_SCREEN, Y_FLOOR, GRAVITY) ;
 
 
-					if(plat_manager) {
-						platform_manager_update(plat_manager, 1.0/FPS, X_SCREEN) ;
-					}
+                    if(plat_manager) {
+                        platform_manager_update(plat_manager, 1.0/FPS, X_SCREEN) ;
+                    }
 
-					if(p1->health == 0) {
-						p1->alive = false ;
-						estado = GAMEOVER ;
-					}
-					if(p1->win) {
-						estado = VICTORY;
-					}
-				}
-				redraw = true ;
-			break;
+                    if(p1->health == 0) {
+                        p1->alive = false ;
+                        estado = GAMEOVER ;
+                    }
+                    if(p1->win) {
+                        estado = VICTORY;
+                    }
+                }
+                redraw = true ;
+            break;
 
-			case ALLEGRO_EVENT_KEY_DOWN :
+            case ALLEGRO_EVENT_KEY_DOWN :
 
-			    switch (estado)
-				{
-					case GAMEPLAY : 
-						wasd_keys(p1, &event) ;
+                switch (estado)
+                {
+                    case GAMEPLAY : 
+                        wasd_keys(p1, &event) ;
 
-					break;
+                    break;
 
-					case MENU:
+                    case MENU:
 
-						if (event.keyboard.keycode == ALLEGRO_KEY_UP) 
-						    menu_select = 1;
-						else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) 
-							menu_select = 2;
-						else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-							if (menu_select == 1) {
-								estado = GAMEPLAY;
-							} 
-							else {
-								done = true;
-							}
-						}
-					break;
+                        if (event.keyboard.keycode == ALLEGRO_KEY_UP) 
+                            menu_select = 1;
+                        else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) 
+                            menu_select = 2;
+                        else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
+                            if (menu_select == 1) {
+                                estado = GAMEPLAY;
+                            } 
+                            else {
+                                done = true;
+                            }
+                        }
+                    break;
 
-					case GAMEOVER :
+                    case GAMEOVER :
 
-						if (event.keyboard.keycode == ALLEGRO_KEY_UP) 
-						    menu_select = 1;
-						else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) 
-							menu_select = 2;
-						else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-							if (menu_select == 1) {
+                        if (event.keyboard.keycode == ALLEGRO_KEY_UP) 
+                            menu_select = 1;
+                        else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) 
+                            menu_select = 2;
+                        else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
+                            if (menu_select == 1) {
 
-								obstacle_manager_reset_all(obs_manager, X_SCREEN, Y_FLOOR) ;
-								game_clean(p1, floor, obs_manager, plat_manager) ;
-								game_set(&p1, &floor, &obs_manager, &plat_manager) ;
-								estado = GAMEPLAY;
-							} 
-							else {
-								done = true;
-							}
-						}
-					break;
+                                obstacle_manager_reset_all(obs_manager, X_SCREEN, Y_FLOOR) ;
+                                game_clean(p1, floor, obs_manager, plat_manager) ;
+                                game_set(&p1, &floor, &obs_manager, &plat_manager) ;
+                                estado = GAMEPLAY;
+                            } 
+                            else {
+                                done = true;
+                            }
+                        }
+                    break;
 
-					case VICTORY:
+                    case VICTORY:
 
-						if (event.keyboard.keycode == ALLEGRO_KEY_UP) 
-						    menu_select = 1;
-						else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) 
-							menu_select = 2;
-						else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-							if (menu_select == 1) {
+                        if (event.keyboard.keycode == ALLEGRO_KEY_UP) 
+                            menu_select = 1;
+                        else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) 
+                            menu_select = 2;
+                        else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
+                            if (menu_select == 1) {
 
-								obstacle_manager_reset_all(obs_manager, X_SCREEN, Y_FLOOR) ;
-								game_clean(p1, floor, obs_manager, plat_manager) ;
-								game_set(&p1, &floor, &obs_manager, &plat_manager) ;
-								estado = GAMEPLAY;
-							} 
-							else {
-								done = true;
-							}
-						}
-					break;
-						
-					redraw = true ;
-				}
+                                obstacle_manager_reset_all(obs_manager, X_SCREEN, Y_FLOOR) ;
+                                game_clean(p1, floor, obs_manager, plat_manager) ;
+                                game_set(&p1, &floor, &obs_manager, &plat_manager) ;
+                                estado = GAMEPLAY;
+                            } 
+                            else {
+                                done = true;
+                            }
+                        }
+                    break;
+                        
+                    redraw = true ;
+                }
 
-			break;
+            break;
 
-			case ALLEGRO_EVENT_KEY_UP :
-				wasd_keys(p1, &event) ;
-				
-			break ;
+            case ALLEGRO_EVENT_KEY_UP :
+                wasd_keys(p1, &event) ;
+                
+            break ;
 
-			case ALLEGRO_EVENT_DISPLAY_CLOSE :
-				done = true ;
-			break ;
-			
-		}
-			
-		//desenho 
-		if (redraw && al_is_event_queue_empty(queue)) {
-			redraw = false ;
+            case ALLEGRO_EVENT_DISPLAY_CLOSE :
+                done = true ;
+            break ;
+            
+        }
+            
+        //desenho 
+        if (redraw && al_is_event_queue_empty(queue)) {
+            redraw = false ;
 
-			switch (estado)
-			{
-				case MENU:
-					al_clear_to_color(al_map_rgb(0, 0, 0));
-					draw_menu(game_bg_img, font, menu_select) ;
-				
-				break;
+            switch (estado)
+            {
+                case MENU:
+                    al_clear_to_color(al_map_rgb(0, 0, 0));
+                    draw_menu(game_bg_img, font, menu_select) ;
+                
+                break;
 
-				case GAMEOVER:
-					al_clear_to_color(al_map_rgb(0, 0, 0));
-					draw_gameover(game_bg_img, font, menu_select) ;
-				
-				break;
+                case GAMEOVER:
+                    al_clear_to_color(al_map_rgb(0, 0, 0));
+                    draw_gameover(game_bg_img, font, menu_select) ;
+                
+                break;
 
-				case VICTORY:
-					al_clear_to_color(al_map_rgb(0, 0, 0));
-					draw_victory(game_bg_img, font, menu_select) ;
-				
-				break;
+                case VICTORY:
+                    al_clear_to_color(al_map_rgb(0, 0, 0));
+                    draw_victory(game_bg_img, font, menu_select) ;
+                
+                break;
 
-				case GAMEPLAY : 
-					al_clear_to_color(al_map_rgb(0, 0, 0));
-					draw_gameplay(game_bg_img, p1, floor, plat_manager) ;
-					obstacle_manager_draw(obs_manager) ;
+                case GAMEPLAY : 
+                    al_clear_to_color(al_map_rgb(0, 0, 0));
+                    draw_gameplay(game_bg_img, p1, floor, plat_manager) ;
+                    obstacle_manager_draw(obs_manager) ;
 
-				break;
-			}
-			al_flip_display() ;
-		}
-	}
+                break;
+            }
+            al_flip_display() ;
+        }
+    }
 
     al_destroy_font(font);	
     al_destroy_display(disp);	
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
     if (game_bg_img) al_destroy_bitmap(game_bg_img);  
-	player_destroy(p1) ;
-	obstacle_manager_destroy(obs_manager) ;
-	platform_manager_destroy(plat_manager) ;
+    player_destroy(p1) ;
+    obstacle_manager_destroy(obs_manager) ;
+    platform_manager_destroy(plat_manager) ;
 
-	return 0;
+    return 0;
 }
